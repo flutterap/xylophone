a91972019d76c7b69d60ee29c83bf00c4f4044b7
Bishal Tureha <bishaltureha123@gmail.com>
2022-11-07 08:54:57 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playsound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildkey({color, soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: color),
+        onPressed: () {
+          final player = AudioPlayer();
+          playsound(soundNumber);
+        },
+        child: Text(''),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildkey(color: Colors.red, soundNumber: 1),
+              buildkey(color: Colors.orange, soundNumber: 2),
+              buildkey(color: Colors.yellow, soundNumber: 3),
+              buildkey(color: Colors.green, soundNumber: 4),
+              buildkey(color: Colors.teal, soundNumber: 5),
+              buildkey(color: Colors.blue, soundNumber: 6),
+              buildkey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
